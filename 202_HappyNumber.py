af2d22f9a7644628231efdd50366d8d4eb775b38
Brett Chien <brett.chien@gmail.com>
2015-06-30 17:21:22 -0700

    202 update: with loop detection

--- 202_HappyNumber.py
+++ 202_HappyNumber.py
@@ -2,6 +2,43 @@ class Solution:
     # @param {integer} n
     # @return {boolean}
     def isHappy(self, n):
+        number = n
+        loop = [number]
+        result = 0
+        while True:
+            m = number % 10
+            result += m ** 2
+            number = number / 10
+            if number == 0:
+                if result == 1:
+                    return True
+                else:
+                    if result in loop:
+                        return False
+                    else:
+                        loop.append(result)
+                        number = result
+                        result = 0
+
+
+    def isHappy_2nd(self, n):
+        numbers = str(n)
+        loop = [numbers]
+        while True:
+            result = 0
+            for d in numbers:
+                result += int(d) ** 2
+            if result == 1:
+                return True
+            else:
+                numbers = str(result)
+                if numbers in loop:
+                    return False
+                else:
+                    loop.append(numbers)
+
+
+    def isHappy_first(self, n):
         MAX_ITERATION = 100
         number = n
         result = 0
@@ -22,4 +59,4 @@ class Solution:
  
 if __name__ == "__main__":
     sol = Solution()
-    print sol.isHappy(7) 
+    print sol.isHappy(19) 
