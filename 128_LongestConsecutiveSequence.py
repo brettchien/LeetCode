ef13b142363131a6a3d1d3d0dae530d990b517f2
Brett Chien <brett.chien@gmail.com>
2015-07-02 02:19:33 -0700

    add dictionary solution for 128

--- 128_LongestConsecutiveSequence.py
+++ 128_LongestConsecutiveSequence.py
@@ -2,6 +2,26 @@ class Solution:
     # @param {integer[]} nums
     # @return {integer}
     def longestConsecutive(self, nums):
+        # use dictionary
+        # h = dict.fromkeys(nums)
+        h = dict([key, None] for key in nums)
+        m = 0
+        while h:
+            key, value = h.popitem()
+            start = end = key
+            #start = end = h.keys().pop()
+            #del h[start]
+            while start - 1 in h:
+                start -= 1
+                del h[start]
+            while end + 1 in h:
+                end += 1
+                del h[end]
+            m = max(m, end - start + 1)
+        return m
+        
+    def alongestConsecutive(self, nums):
+        # use set
         nums = set(nums)
         m = 0
         while nums:
@@ -18,3 +38,4 @@ class Solution:
 if __name__ == "__main__":
     sol = Solution()
     print sol.longestConsecutive([100, 4, 200, 1, 3, 2]) == 4
+    print sol.longestConsecutive([0, 1]) == 2
