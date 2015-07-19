8b6cbae0004472b2a549e9c30c5e711459378ffa
Brett Chien <brett.chien@gmail.com>
2015-07-18 22:25:48 -0700

    add LinkedList data structure

---
+++ LinkedList.py
@@ -0,0 +1,19 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+def toList(ln):
+    result = []
+    while ln:
+        reault.append(ln.val)
+        ln = ln.next
+    return result
+
+def toLinkedList(aList):
+    ln = ListNode(None)
+    curr = ln
+    for val in aList:
+        curr.next = ListNode(val)
+        curr = curr.next
+    return ln.next
